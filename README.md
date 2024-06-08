15fc36ffc5470b20d3423db42246eba1b7f0c016
stuckYogurt <vladimirtsupin@gmail.com>
2024-06-08 13:42:02 +0300

    Road is now moving

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Simple racing game where you need to drive your car and try not to get hit by others.
 
 # todo
+- interface
 - when on the edge of the road starts timer for failure
 - fail screen
 - movable side objects
@@ -10,3 +11,5 @@ Simple racing game where you need to drive your car and try not to get hit by ot
 - speed and nitro
 - level up
 - fps doesn't affect speed
+- another type of sterring
+- random img chooser of cars and surroundings is not dependent of file names and their quantity is chosen automatically
--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const version = 'v. 0.0.1';
+const version = 'v. 0.0.2';
 document.getElementById('version').innerHTML = version;
 
 let fpsLimit = 60;
 let frameTimeLimit = Math.floor(1000/fpsLimit); // in ms
 
-
+// [x; y]
+function getRandomInt(floorInt,ceilInt) {
+    return Math.floor(Math.random()*(ceilInt+1)) + floorInt;
+}
 
 
 class Car {
@@ -56,16 +59,53 @@ class Car {
             }
         });
     }
-}
+};
 
-// Car movement set up
+
+
+class Marking {
+
+    constructor(x, y, markInterval, parentReference, markHeight) {
+        this.x = x;
+        this.y = y;
+        this.markInterval = markInterval;
+        this.markHeight = markHeight;
+
+        this.parentReference = parentReference;
+        this.markReference = document.createElement('div');
+        this.markReference.className = 'mark';
+        this.markReference.style.height = String(Math.round(markHeight))+'px'
+        this.markReference.style.left = String(this.x)+'px';
+        this.markReference.style.bottom = String(this.y)+'px';
+        this.parentReference.append(this.markReference);
+    }
+
+    isVisible(highestMark) {
+        console.log(window.innerHeight - highestMark);
+        console.log(this.markInterval);
+        if (this.y< -this.markHeight &&(window.innerHeight - highestMark) >= this.markInterval) {
+            this.y = window.innerHeight;
+            this.markReference.style.bottom = String(this.y)+'px';
+        };
+    }
+
+    moveSelf(diff) {
+        this.y = this.y - diff;
+        this.markReference.style.bottom = String(this.y)+'px';
+    }
+};
+
+
+
+
+
 
 
 
 class Frames {
     constructor() {
         this.started = false;
-
+        this.gameSpeed = 10;
         document.addEventListener('click', ()=>{
             if (this.started) {
                 clearInterval(this.gameFrameInterval);
@@ -93,10 +133,32 @@ class Frames {
             // Key binds
             'ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'
         );
+
+        // other objects
+        this.movableMarking = []
+        this.others = []
+
+        // Objects Generation
+        this.roadLineRef = document.getElementsByClassName('static')[0];
+        let y = 0;
+        let markingInterval = 10; // in vh
+        let markWidth = 2; // in vh
+        let markHeight = 20; // in vh
+        while (y<window.innerHeight) {
+            this.movableMarking.push(new Marking(
+                window.innerWidth * 0.5 - 0.5 * markWidth/100 * window.innerHeight, y, 
+                markingInterval*window.innerHeight/100, this.roadLineRef, markHeight));  
+            y+=markingInterval*window.innerHeight/100;
+        };
+
     }
-    // Указывать через this ?
+
+    createMarking() {
+
+    }
+
+
     frame() {
-        console.log(this.car.x);
         // Movement
         if (frames.car.left) {
             this.car.x = this.car.x - this.car.steerSpeed;
@@ -114,9 +176,20 @@ class Frames {
             this.car.y = this.car.y - this.car.breakSpeed;
             this.car.carPageReference.style.bottom = String(this.car.y) + 'px';
         };
+
+        let highestMark = this.movableMarking[0].y;
+        for (let i = 1; i < this.movableMarking.length; i++) {
+            highestMark = Math.max(this.movableMarking[i].y, highestMark);
+        }
+
+        for (let i = 0; i < this.movableMarking.length; i++) {
+            this.movableMarking[i].moveSelf(this.gameSpeed);
+            this.movableMarking[i].isVisible(highestMark);
+        }
+
+
     }
     startFraming() {
-        console.log(this.car);
         this.gameFrameInterval = setInterval(this.frame.bind(this), frameTimeLimit);
     }
 }
